e4cb5b7e36145f9cc7d96d79295a589be3ab3b2d
taingo956 <taingo956@gmail.com>
2026-01-14 00:17:04 +0700

    đã sửa chữa

--- main.cpp
+++ main.cpp
@@ -5,15 +5,18 @@ using namespace std;
 //TODO Tao bang min
 void generateBombMap(int board[],int rows,int cols,int bombcount)  {
     int  size = rows * cols;
-    for (int  i = 0; i < rows * cols; i++){
+    for (int  i = 0; i < rows * cols; i++)
+    {
         board[i]= 0;
     }
     srand(time(NULL));
     int count = 0;
-    if ( bombcount > size) {
+    if ( bombcount > size) 
+    {
         bombcount = size;
     }
-    while ( count < bombcount ) {
+    while ( count < bombcount ) 
+    {
         int pos = rand() % size;
     }
     if (board[pos] == 0) {
@@ -21,25 +24,34 @@ void generateBombMap(int board[],int rows,int cols,int bombcount)  {
         count++;
     }
 }
+
 // TODO Tinh so min lan can
 void calculateBombNumbers(int board[], int rows, int cols) {
-    for (int r = 0; r < rows; r++) {
-        for (int c = 0; c < cols; c++) {
+    for (int r = 0; r < rows; r++) 
+    {
+        for (int c = 0; c < cols; c++) 
+        {
             int index = r * cols + c;
-            if (board[index] == -1) {
+            if (board[index] == -1) 
+            {
                 continue;
             }
             int bombcount = 0;
-            for (int i = -1; i <= 1; i++) {
-                for (int j = -1; j <= 1;j++) {
-                    if (i == 0 && j == 0) {
+            for (int i = -1; i <= 1; i++) 
+            {
+                for (int j = -1; j <= 1;j++) 
+                {
+                    if (i == 0 && j == 0) 
+                    {
                         continue;
                     }
                     int newrow = r + i;
                     int newcol = c + j;
-                    if (newrow >= 0 && newrow < rows && newcol >= 0 && newcol < cols) {
+                    if (newrow >= 0 && newrow < rows && newcol >= 0 && newcol < cols) 
+                    {
                         int newindex = newrow * cols + newcol;
-                        if (board[newindex] == -1) {
+                        if (board[newindex] == -1) 
+                        {
                             bombcount++;
                         }
                     } 
@@ -49,6 +61,7 @@ void calculateBombNumbers(int board[], int rows, int cols) {
         board[index] = bombcount;
     }
 }
+
 int main(){
     return 0;
 }

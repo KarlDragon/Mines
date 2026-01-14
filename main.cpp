409a2220136208c39cd71838d351f50a7ebe794d
taingo956 <taingo956@gmail.com>
2026-01-14 15:31:59 +0700

    chỉnh sửa

--- main.cpp
+++ main.cpp
@@ -3,17 +3,21 @@
 #include <ctime>
 using namespace std;
 // TODO Tao bang min
-void generateBombMap(int board[],int rows,int cols,int bombcount)  {
+void generateBombMap(int board[],int rows,int cols,int bombcount)  
+{
     int size = rows * cols;
-    for (int  i = 0; i < rows * cols; i++){
+    for (int  i = 0; i < rows * cols; i++)
+    {
         board[i] = 0;
     }
     srand(time(NULL));
     int count = 0;
-    if ( bombcount > size) {
+    if ( bombcount > size) 
+    {
         bombcount = size;
     }
-    while ( count < bombcount ) {
+    while ( count < bombcount ) 
+    {
         int pos = rand() % size;
         if (board[pos] == 0) {
             board[pos] = -1;

7d4d6b599719e83b6711a3aa067a78d1bf919e0b
taingo956 <taingo956@gmail.com>
2026-01-14 15:43:42 +0700

    hoàn thành

--- main.cpp
+++ main.cpp
@@ -28,41 +28,7 @@ void generateBombMap(int board[],int rows,int cols,int bombcount)
 
 // TODO Tinh so min lan can
 void calculateBombNumbers(int board[], int rows, int cols) 
-{
-    for (int r = 0; r < rows; r++) 
-    {
-        for (int c = 0; c < cols; c++) 
-        {
-            int index = r * cols + c;
-            if (board[index] == -1) 
-            {
-                continue;
-            }
-            int bombcount = 0;
-            for (int i = -1; i <= 1; i++) 
-            {
-                for (int j = -1; j <= 1;j++) 
-                {
-                    if (i == 0 && j == 0) 
-                    {
-                        continue;
-                    }
-                    int newrow = r + i;
-                    int newcol = c + j;
-                    if (newrow >= 0 && newrow < rows && newcol >= 0 && newcol < cols) 
-                    {
-                        int newindex = newrow * cols + newcol;
-                        if (board[newindex] == -1) 
-                        {
-                            bombcount++;
-                        }
-                    } 
-                }
-            }
-        }
-        board[index] = bombcount;
-    }
-}
+
 
 int main(){
     return 0;

c8ab781f79a33b3f6db3ff5b15b7a0f4cc97aa80
taingo956 <taingo956@gmail.com>
2026-01-15 12:31:02 +0700

    đã sửa chữa còn lỗi nào k

--- main.cpp
+++ main.cpp
@@ -4,65 +4,57 @@
 #include <vector>
 using namespace std;
 //TODO Tao bang min
-void generateBombMap(int board[],int rows,int cols,int bombcount)
-{
-    int  size = rows * cols;
-    for (int  i = 0; i < rows * cols; i++)
-    {
-        board[i]= 0;
-    }
-    srand(time(NULL));
-    int count = 0;
-    if ( bombCount > size) 
-    {
-        bombCount = size;
-    }
-    while ( count < bombCount ) 
-    {
-        int pos = rand() % size;
-        if (board[pos] == 0) 
-        {
-            board[pos] = -1;
-            count++;
+vector<vector<int>> generateBombMap(int rows, int cols, int bombCount) {
+    vector<vector<int>> board(rows, vector<int>(cols, 0));
+
+    srand(time(nullptr));
+    int placedBombs = 0;
+
+    while (placedBombs < bombCount) {
+        int r = rand() % rows;
+        int c = rand() % cols;
+
+        if (board[r][c] == 0) {
+            board[r][c] = -1;
+            placedBombs++;
         }
     }
+
+    return board;
 }
 // TODO Tinh so min lan can
-void calculateBombNumbers(int board[], int rows, int cols) 
-{
-    for (int r = 0; r < rows; r++) 
-    {
-        for (int c = 0; c < cols; c++) 
-        {
-            int index = r * cols + c;
-            if (board[index] == -1) 
-            {
-                continue;
-            }
+vector<vector<int>> calculateBombNumbers(const vector<vector<int>>& board) {
+    int rows = board.size();
+    int cols = board[0].size();
+
+    vector<vector<int>> result = board;
+
+    int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+    int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+
+            if (board[i][j] == -1) continue;
+
             int bombCount = 0;
-            for (int i = -1; i <= 1; i++) 
-            {
-                for (int j = -1; j <= 1;j++) 
-                {
-                    if (i == 0 && j == 0) 
-                    {
-                        continue;
+
+            for (int k = 0; k < 8; k++) {
+                int ni = i + dx[k];
+                int nj = j + dy[k];
+
+                if (ni >= 0 && ni < rows && nj >= 0 && nj < cols) {
+                    if (board[ni][nj] == -1) {
+                        bombCount++;
                     }
-                    int newrow = r + i;
-                    int newcol = c + j;
-                    if (newrow >= 0 && newrow < rows && newcol >= 0 && newcol < cols) 
-                    {
-                        int newindex = newrow * cols + newcol;
-                        if (board[newindex] == -1) 
-                        {
-                            bombCount++;
-                        }
-                    } 
                 }
             }
-            board[index] = bombCount;
+
+            result[i][j] = bombCount;
         }
     }
+
+    return result;
 }
 //Mang Revealed
 vector<vector<bool>> createRevealed(int row, int column) {
